36aa73029c37ed96a42ddb67e8aaeaa6849c39e8
BrightonDutson <102453602+BrightonDutson@users.noreply.github.com>
2022-04-22 10:17:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<?php
+$NAME=$_COOKIE['$cookie_name'];
+if(isset($NAME))
+{
+  if($NAME=='fredchecker')
+  {
+    header("Location: page.html"); 
+  } else{
+    header("Location: http://thecalculatorguy.ga/F.R.E.D./403.html");
+  }
